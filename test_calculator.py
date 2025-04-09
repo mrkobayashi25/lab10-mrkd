86eaf698829f69e0d223ea8cd5f309b9e9c9f972
Yuga <yuga.tennis1997@icloud.com>
2025-04-09 14:21:22 -0400

    included all required unit tests

--- test_calculator.py
+++ test_calculator.py
@@ -7,6 +7,17 @@ from calculator import *
 
 class TestCalculator(unittest.TestCase):
 
+    # Partner's responsibilities
+    def test_add(self):
+        self.assertEqual(add(2, 3), 5)
+
+    def test_subtract(self):
+        self.assertEqual(subtract(7, 4), 3)
+
+    def test_logarithm(self):
+        self.assertAlmostEqual(logarithm(10, 100), 2.0)
+
+    # Your responsibilities
     def test_multiply(self):
         self.assertEqual(mul(3, 4), 12)
 
