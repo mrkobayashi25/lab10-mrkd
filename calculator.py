3649adaa425eef30a9d8479db126273b2e2c542e
Isabelle Sundin <sundin.i@ufl.edu>
2025-04-11 15:57:51 -0400

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -26,7 +26,7 @@ def div(a, b):
         raise ZeroDivisionError("Cannot divide by zero")
     return b / a
 
-def log(a, b):
+def logarithm(a, b):
     if a <= 0 or a == 1 or b <= 0:
         raise ValueError("Invalid input for logarithm")
     return math.log(b, a)
