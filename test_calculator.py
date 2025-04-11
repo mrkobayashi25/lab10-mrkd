9364b0f6b4db41f6b0656fe7e8a36c79c7256be3
Isabelle Sundin <sundin.i@ufl.edu>
2025-04-11 16:05:44 -0400

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -37,35 +37,30 @@ class TestCalculator(unittest.TestCase):
 #Partner 2's test functions
 
     def test_add(self):
-        assert add(2, 3) == 5
-        assert add(-1, -1) == -2
-        assert add(0, 0) == 0
+        self.assertEqual(add(2, 3), 5)
+        self.assertEqual(add(-1, -1), -2)
+        self.assertEqual(add(0, 0), 0)
         print("test_add passed")
 
     def test_substract(self):
-        assert sub(5, 2) == 3
-        assert sub(0, 3) == -3
-        assert sub(-2, -2) = 0
+        self.assertEqual(sub(5, 3), 2)
+        self.assertEqual(sub(0, 3), -3)
+        self.assertEqual(sub(-2, -2), 0)
         print("test_sub passed")
 
     def test_divide_by_zero(self):
-        try:
-            div(0, 5)
-        except ZeroDivisionError as e:
-            print("test_divide_by_zero passed.")
-        else:
-            print("test_divide_by_zero failed.")
+       with self.assertRaises(ZeroDivisionError):
+           div(0, 5)
 
     def test_logarithm(self):
-        assert round(log(10, 100), 6) == 2
-        assert round(log(2, 8), 6) == 3
+        self.assertEqual(round(log(10, 100), 6), 2)
+        self.assertEqual(round(log(2, 8), 6), 3)
         print("test_logarithm passed.")
 
     def test_log_invalid_base(self):
-        try:
+        with self.assertRaises(ValueError):
             log(1, 10)
-        except ValueError:
-            print("test_log_invalid_base passed.")
-        elseL
-            print("test_log_invalid_base failed.")
-        
+        with self.assertRaises(ValueError):
+            log(-2, 10)
+        with self.assertRaises(ValueError):
+            log(10, -2)
