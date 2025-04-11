a6cbc8bd1796632ba33ceacb86a0d82f1d0d6322
Isabelle Sundin <sundin.i@ufl.edu>
2025-04-11 15:50:01 -0400

    Part2 finished_test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -51,3 +51,39 @@ class TestCalculator(unittest.TestCase):
         self.assertAlmostEqual(square_root(9), 3.0)
         with self.assertRaises(ValueError):
             square_root(-4)
+
+#Partner 2's test functions
+
+    def test_add():
+        assert add(2, 3) == 5
+        assert add(-1, -1) == -2
+        assert add(0, 0) = 0
+        print("test_add passed")
+
+    def test_substract():
+        assert sub(5, 2) == 3
+        assert sub(0, 3) == -3
+        assert sub(-2, -2) = 0
+        print("test_sub passed")
+
+    def test_divide_by_zero():
+        try:
+            div(0, 5)
+        except ZeroDivisionError as e:
+            print("test_divide_by_zero passed.")
+        else:
+            print("test_divide_by_zero failed.")
+
+    def test_logarithm():
+        assert round(log(10, 100), 6) == 2
+        assert round(log(2, 8), 6) == 3
+        print("test_logarithm passed.")
+
+    def test_log_invalid_base():
+        try:
+            log(1, 10)
+        except ValueError:
+            print("test_log_invalid_base passed.")
+        elseL
+            print("test_log_invalid_base failed.")
+        
