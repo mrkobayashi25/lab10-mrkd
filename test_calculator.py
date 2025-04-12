6e4a7bbf7e0c643903c16b57288f306ec1b8f2d1
Mason Kobayashi Downs <145729998+mrkobayashi25@users.noreply.github.com>
2025-04-12 08:22:40 -0400

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -40,10 +40,10 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(add(-1, -1), -2)
         self.assertEqual(add(0, 0), 0)
 
-def test_subtract(self):
-    self.assertEqual(subtract(5, 3), 2)
-    self.assertEqual(subtract(0, 3), -3)
-    self.assertEqual(subtract(-2, -2), 0)
+    def test_subtract(self):
+        self.assertEqual(subtract(5, 3), 2)
+        self.assertEqual(subtract(0, 3), -3)
+        self.assertEqual(subtract(-2, -2), 0)
 
     def test_exp(self):
         self.assertEqual(exp(2, 3), 8)
@@ -60,3 +60,6 @@ def test_subtract(self):
             logarithm(-2, 10)
         with self.assertRaises(ValueError):
             logarithm(10, -2)
+
+if __name__ == "__main__":
+    unittest.main()
